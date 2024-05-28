ad12dc5864c457bc5730d23b4f999b0199f3b0e2
gthakurx <gthakurx@gmail.com>
2024-05-28 16:20:17 +0530

    Ecom App

---
+++ ecom/ecom/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class User(models.Model):
+    name=models.CharField(max_length=255)
+    email=models.EmailField(max_length=254)
+    password=models.CharField( max_length=255)
+    # shipping_address=models.TextField()
+    # billing_address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Address(models.Model):
+    street=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    state=models.CharField(max_length=255)
+    zip_code=models.CharField(max_length=255)
+    country=models.CharField(max_length=255)
+    #should we create a Address class abstract or should we create a seperate Table for that
+    class Meta:
+        abstract=True
+
+class Shipping_Address(Address):
+    user=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
